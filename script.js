886d29a1a1bf17e0714e2c64de17065d8faa965f
eduardolima002 <eduardosalviano002@hotmail.com>
2023-08-22 06:46:42 -0300

    adicionado playerSpotify

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ let inputRain = toggleRain.querySelector('input');
 toggleDay.addEventListener('click', toogleB);
 toggleRain.addEventListener('click', toogleB);
 
-function toogleB(){
+function toogleB() {
 
-    if(inputDay.checked && (!inputRain.checked)){
+    if (inputDay.checked && (!inputRain.checked)) {
         document.querySelector('html').style.backgroundImage = 'url("./imagens/fotonoite.png")'
-    }else if(!inputDay.checked && (!inputRain.checked)){
+    } else if (!inputDay.checked && (!inputRain.checked)) {
         document.querySelector('html').style.backgroundImage = 'url("./imagens/fotodia.png")'
-    }else if(inputDay.checked && (inputRain.checked)){
+    } else if (inputDay.checked && (inputRain.checked)) {
         document.querySelector('html').style.backgroundImage = 'url("./imagens/melhor-noite.gif")'
-    }else if(!inputDay.checked && (inputRain.checked)){
+    } else if (!inputDay.checked && (inputRain.checked)) {
         document.querySelector('html').style.backgroundImage = 'url("./imagens/melhor.gif")'
     }
 }
+
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+        uri: 'spotify:playlist:37i9dQZF1DWWQRwui0ExPn'
+    };
+    const callback = (EmbedController) => { };
+    IFrameAPI.createController(element, options, callback);
+};
